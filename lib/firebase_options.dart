f4dd540a05b5b373a08c39a0559e35017496f7c2
Snehahande05 <snehahande175@gmail.com>
2026-09-10 14:44:42 +0530

    commit

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -4,44 +4,40 @@ import 'package:flutter/foundation.dart'
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      return web;
-    }
+    if (kIsWeb) return web;
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
-      case TargetPlatform.windows:
-        return windows;
+      case TargetPlatform.iOS:
+        return ios;
       default:
         return web;
     }
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCh1EiLz6ze0pdGvuDoUa511K-cqR76jdg',
-    appId: '1:1093007023409:web:62fb6deaa1ac674ec3e410',
-    messagingSenderId: '1093007023409',
-    projectId: 'fir-auth-be3a9',
-    authDomain: 'fir-auth-be3a9.firebaseapp.com',
-    storageBucket: 'fir-auth-be3a9.firebasestorage.app',
-    measurementId: 'G-GZYGKKMDVT',
+    apiKey: 'AIzaSyB_1TfCfWsuwI3LrE-WNLgB8Ndko1RBQ2g',
+    appId: '1:1037440525016:web:e3cf795f364f546663edad',
+    messagingSenderId: '1037440525016',
+    projectId: 'flowerapp-a0865',
+    authDomain: 'flowerapp-a0865.firebaseapp.com',
+    storageBucket: 'flowerapp-a0865.firebasestorage.app',
+    measurementId: 'G-SPQK89G5QW',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCh1EiLz6ze0pdGvuDoUa511K-cqR76jdg',
-    appId: '1:1093007023409:android:62fb6deaa1ac674ec3e410',
-    messagingSenderId: '1093007023409',
-    projectId: 'fir-auth-be3a9',
-    storageBucket: 'fir-auth-be3a9.firebasestorage.app',
+    apiKey: 'AIzaSyB_1TfCfWsuwI3LrE-WNLgB8Ndko1RBQ2g',
+    appId: '1:1037440525016:web:e3cf795f364f546663edad',
+    messagingSenderId: '1037440525016',
+    projectId: 'flowerapp-a0865',
+    storageBucket: 'flowerapp-a0865.firebasestorage.app',
   );
 
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyCh1EiLz6ze0pdGvuDoUa511K-cqR76jdg',
-    appId: '1:1093007023409:web:62fb6deaa1ac674ec3e410',
-    messagingSenderId: '1093007023409',
-    projectId: 'fir-auth-be3a9',
-    authDomain: 'fir-auth-be3a9.firebaseapp.com',
-    storageBucket: 'fir-auth-be3a9.firebasestorage.app',
-    measurementId: 'G-GZYGKKMDVT',
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyB_1TfCfWsuwI3LrE-WNLgB8Ndko1RBQ2g',
+    appId: '1:1037440525016:ios:e3cf795f364f546663edad',
+    messagingSenderId: '1037440525016',
+    projectId: 'flowerapp-a0865',
+    storageBucket: 'flowerapp-a0865.firebasestorage.app',
   );
 }
